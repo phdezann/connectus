72b1024ad5c23fffa576d27afef8b470b05b3338
Pierre-Henri Dezanneau <ph.dezanneau@gmail.com>
2016-01-25 17:19:33 +0100

    Docker is used to fully build and deploy the project.

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-docker build -t connectus-build -f Dockerfile.build .
-docker build -t connectus-build-cache -f Dockerfile.build.cache .
-docker create --name connectus-build-cache connectus-build-cache /bin/bash || echo "Data container for build caches already exists, keep it for faster build executions."
-docker run -it --rm --volumes-from connectus-build-cache -v $(pwd):/workspace -w /workspace/backend connectus-build sbt stage
-docker run -it --rm --volumes-from connectus-build-cache -v $(pwd):/workspace -w /workspace/android-app connectus-build ./gradlew assembleDebug
