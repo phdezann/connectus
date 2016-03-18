431471dc251f8da8efd359160de5dcecfc1c8f1a
Pierre-Henri Dezanneau <ph.dezanneau@gmail.com>
2016-03-18 15:09:55 +0100

    Minor tweak to get the app running on heroku.

--- connectus-backend/app/support/Module.scala
+++ connectus-backend/app/support/Module.scala
@@ -4,13 +4,14 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier
 import com.google.api.client.http.javanet.NetHttpTransport
 import com.google.api.client.json.jackson2.JacksonFactory
 import com.google.api.client.util.store.AbstractDataStoreFactory
-import play.api.inject._
-import play.api.{Configuration, Environment}
+import com.google.inject.AbstractModule
+import controllers.AppController
 import services.RepositoryDataStoreFactory
 
-class AppModule extends Module {
-  def bindings(env: Environment, conf: Configuration) = Seq(
-    bind[AbstractDataStoreFactory].to(classOf[RepositoryDataStoreFactory]),
-    bind[GoogleIdTokenVerifier].to(new GoogleIdTokenVerifier.Builder(new NetHttpTransport, new JacksonFactory).build)
-  )
+class AppModule extends AbstractModule {
+  def configure() = {
+    bind(classOf[AppController]).asEagerSingleton // in prod mode all dependencies of AppController get built at startup time
+    bind(classOf[AbstractDataStoreFactory]).to(classOf[RepositoryDataStoreFactory])
+    bind(classOf[GoogleIdTokenVerifier]).toInstance(new GoogleIdTokenVerifier.Builder(new NetHttpTransport, new JacksonFactory).build)
+  }
 }
--- connectus-backend/build.sbt
+++ connectus-backend/build.sbt
@@ -1,4 +1,4 @@
-name := """play-getting-started"""
+name := """connectus"""
 
 version := "1.0-SNAPSHOT"
 
