783361fc080aa8f499601e726e098fdf4287574d
Pierre-Henri Dezanneau <ph.dezanneau@gmail.com>
2016-05-27 11:29:14 +0200

    Cleanup.

--- connectus-android-app/app/src/main/java/org/connectus/ResidentAddDialogFragment.java
+++ connectus-android-app/app/src/main/java/org/connectus/ResidentAddDialogFragment.java
@@ -8,15 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
 
-import javax.inject.Inject;
-
 public class ResidentAddDialogFragment extends DialogFragment {
-
-    @Inject
-    UserRepository userRepository;
-
-    @Inject
-    Repository repository;
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -25,10 +17,10 @@ public class ResidentAddDialogFragment extends DialogFragment {
         View dialogLayout = inflater.inflate(R.layout.resident_add_dialog_layout, null);
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setView(dialogLayout);
-        EditText listView = (EditText) dialogLayout.findViewById(R.id.name);
+        EditText nameEditText = (EditText) dialogLayout.findViewById(R.id.name);
         builder.setNegativeButton(android.R.string.cancel, (dialog, id) -> dismiss());
         builder.setPositiveButton(R.string.add, (dialog, id) -> {
-            ((MainActivity) getActivity()).addResidentAndAddContact(listView.getText().toString());
+            ((MainActivity) getActivity()).addResidentAndAddContact(nameEditText.getText().toString());
             dismiss();
         });
         AlertDialog dialog = builder.create();
